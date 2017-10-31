dae8e9f2c5009d5cb681068a140107295a8ec33b
isoeugenol-1 <wichasha13@gmail.com>
2017-10-31 04:52:02 -0700

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,22 @@
 #include "cal.h"
 void display(int a, int b){
 
-    printf("a+b=%d",sum(a,b));
-    printf("a*b=%d",multi(a,b));
+    printf("a+b=%d\n",sum(a,b));
+    printf("a*b=%d\n",multi(a,b));
 }
 
 void main(){
     int a, b;
+
+    printf("print +,* two number\n");
+    printf("insert two number : ");
     scanf("%d %d",&a,&b);
 
  
     display(a,b);
 
 
+    printf("end\n");
 
 
 }
