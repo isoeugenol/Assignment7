4c15d474109dfb1dd5fec9f7451c17ed9b34a1c8
isoeugenol <scopo724@gmail.com>
2017-10-31 04:59:16 -0700

    Cmake build

---
+++ build/CMakeFiles/Assignment7.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment7.dir/main.c.o"
+  "CMakeFiles/Assignment7.dir/cal.c.o"
+  "Assignment7.pdb"
+  "Assignment7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
