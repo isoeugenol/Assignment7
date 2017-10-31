f3fd94290f64def18223a034d3543f9af51ba487
isoeugenol-1 <wichasha13@gmail.com>
2017-10-31 04:42:45 -0700

    Add display func

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "cal.h"
+void display(int a, int b){
 
-
+    printf("a+b=%d",sum(a,b));
+    printf("a*b=%d",multi(a,b));
+}
 
 void main(){
     int a, b;
     scanf("%d %d",&a,&b);
 
-    printf("a*b=%d\n",multi(a,b));
-
-
-    printf("a+b=%d",sum(a,b));
+ 
+    display(a,b);
 
 
 
